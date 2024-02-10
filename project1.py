3ba71f552183348a1e32ff6479fb895f67aa3a1a
jobayerAmahin <150835400+jobayerAmahin@users.noreply.github.com>
2024-02-10 23:24:04 +0200

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,28 @@
+print('Welcome to simple game!')
+
+gaming=input("Do you wanna play?")
+
+if gaming.lower() !="yes":
+    quit()
+
+print('OK. lets play')
+score=0
+
+ans=input('What does DOM stands for?')
+
+if ans=='Document Object Model':
+    print('Correct!')
+    score+=1
+else:
+    print('Incorrect!')
+
+ans1=input('What is Central Processing Unit short form?')
+
+if ans1.upper() =='CPU':
+    print('Correct!')
+    score+=1
+else:
+    print('Incorrect!')
+
+print('You got'+ str(score)+'quiestions correct')
+print('You got'+str((score/4)*100)+'percent %')
